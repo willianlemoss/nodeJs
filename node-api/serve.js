194d1dff82d8994fecd89d7b3eafa9ba42e28940
willianlemoss <willianhophip@gmail.com>
2020-02-07 18:45:52 -0300

    Servidor Finalizado

--- node-api/serve.js
+++ node-api/serve.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const cors = require('cors')
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
+
+mongoose.connect('mongodb+srv://willian:willian@cluster0-zoxtk.mongodb.net/nodeApi?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true ,
+});
+
+requireDir('./src/models');
+
+app.use('/api',require('./src/routes'));
 app.listen(3001);
